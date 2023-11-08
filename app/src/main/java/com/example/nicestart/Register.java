7974b9bfea90c3cb8b8ff66ec6d4ea0124ab5cad
Alex Rodriguez Mazariegos <alexrodriguezmazariegos@gmail.com>
2023-11-08 12:55:57 +0100

    new activity register, landscape mode etc

--- app/src/main/java/com/example/nicestart/Register.java
+++ app/src/main/java/com/example/nicestart/Register.java
@@ -6,24 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
     }
-
     public void openMain(View view){
-        Intent intent = new Intent(Login.this, MainActivity.class);
+        Intent intent = new Intent(Register.this, MainActivity.class);
         //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    public void openRegister(View view){
-        Intent intent = new Intent(Login.this, Register.class);
         startActivity(intent);
     }
 }
